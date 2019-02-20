b9fb3c56af56a687b28e90f5cadb09b1a813fb3f
John Pappas <jaydeepappas@gmail.com>
2019-02-20 02:12:09 -0600

    Lowercase "r" to match interface name

--- src/threesolid.robot.java
+++ src/threesolid.robot.java
@@ -11,7 +11,7 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
-class Robot extends BaseWorker implements IReboot{
+class Robot extends BaseWorker implements Ireboot{
 	public void work() {
 		// ....working
 	}
