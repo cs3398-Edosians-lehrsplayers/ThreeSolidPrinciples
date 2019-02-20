ab63c0179c886d3f958bd930b87dffcbe3cc5c2f
Ozy Vielma <ozviis11@gmail.com>
2019-02-20 00:39:47 -0600

    Ireboot to IReboot

--- src/threesolid.robot.java
+++ src/threesolid.robot.java
@@ -11,7 +11,7 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
-class Robot extends BaseWorker implements Ireboot{
+class Robot extends BaseWorker implements IReboot{
 	public void work() {
 		// ....working
 	}
