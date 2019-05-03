eeeaadfd03580ba59dfd364e8fc9c6ff2017cd68
Ozy Vielma <ozviis11@gmail.com>
2019-05-03 14:08:27 -0500

    Change to TestWorker.java, add successful and failed tests.

--- src/test/java/test<edosians>/TestWorker.java
+++ src/test/java/test<edosians>/TestWorker.java
@@ -11,4 +11,19 @@ public class TestWorker {
     {
         assertEquals(w.work(), "I'm working already!", "Mismatch between test text and method text");
     }
+
+    @Test
+    @DisplayName("Oswaldo's Successful Test")
+    public void newtestOVWorkerPass()
+    {
+        assertTrue(w.addition(1, 4), "Indeed, I can do addition, I am a smart program.");
+    }
+
+    @Test
+    @DisplayName("Oswaldo's Failed Test")
+    public void newtestOVWorkerFail()
+    {
+        
+    }
 }
+
