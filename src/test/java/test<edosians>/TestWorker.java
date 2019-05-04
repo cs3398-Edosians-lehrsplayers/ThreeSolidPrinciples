c064cd1a3059e477659bfc13f5e4ea48167bcbbf
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-05-04 00:04:36 -0500

    run

--- src/test/java/test<edosians>/TestWorker.java
+++ src/test/java/test<edosians>/TestWorker.java
@@ -11,4 +11,18 @@ public class TestWorker {
     {
         assertEquals(w.work(), "I'm working already!", "Mismatch between test text and method text");
     }
+
+    @Test
+    @DisplayName("PASS Method Test")
+    public void newtest_JG_WorkerPass()
+    {
+    assertTrue("THIS WILL PASS", true);
+    }
+
+    @Test
+    @DisplayName("FAIL Method Test")
+    public void newtest_JG_WorkerFail()
+    {
+    assertTrue("THIS WILL FAIL!", false );
+    }
 }
